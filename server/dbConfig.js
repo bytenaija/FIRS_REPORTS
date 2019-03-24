811006185520539d28eedb534ca8528b08ebae5a
Everistus Olumese <everistusolumese@gmail.com>
2019-03-24 12:29:55 +0100

    Update

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-  user: process.env.NODE_ORACLEDB_USER || "SYSTEM",
+  user: process.env.NODE_ORACLEDB_USER || "report_admin",
 
   // Instead of hard coding the password, consider prompting for it,
   // passing it in an environment variable via process.env, or using
   // External Authentication.
-  password: process.env.NODE_ORACLEDB_PASSWORD || 'welcome',
+  password: process.env.NODE_ORACLEDB_PASSWORD || 'welcome1',
 
   // For information on connection strings see:
   // https://oracle.github.io/node-oracledb/doc/api.html#connectionstrings
