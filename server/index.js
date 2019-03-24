9564cbcefc74ddc73c6b3ec3c91521cff8cad86b
Everistus Olumese <everistusolumese@gmail.com>
2019-03-24 10:04:00 +0100

    Update

--- server/index.js
+++ server/index.js
@@ -19,6 +19,48 @@ const NODE_ENV = process.env.NODE_ENV;
 if(NODE_ENV === 'production'){
   app.use(express.static("./client"));
   
+app.get("/api", (req, res) => {
+  try{
+    console.log(dbConfig)
+    oracledb.getConnection({
+        user: dbConfig.user,
+        password: dbConfig.password,
+        connectString: dbConfig.connectString
+      }).then(async conn => {
+      console.log(conn)
+      const currentYearContractSumQuery = 'select contractSubType, count(contractSubType) from reports_vendor group by contractSubType';
+      const currentYearSpendAreasQuery = 'select contractSubType, paymentType, count(contractSubType) from reports_vendor group by contractSubType group by paymentType';
+      const contractAwardedForYearsQuery = 'select contractSubType, trunc(requestDate, "Year") from reports_vendor group by contractSubType group by trunc(requestDate, "Year")'
+  
+      const currentYearContractSum = await conn.execute(currentYearContractSumQuery),
+        currentYearSpendAreas = await conn.execute(currentYearSpendAreasQuery),
+        liabilitiesInSpendAreas = [],
+        fecAwardedProjectsCurrentLiabilities = [],
+        contractAwardedForYears = await conn.execute(contractAwardedForYearsQuery),
+        yearsContractSumVariousSpend = [];
+  
+      res.json({
+        currentYearContractSum,
+        currentYearSpendAreas,
+        liabilitiesInSpendAreas,
+        fecAwardedProjectsCurrentLiabilities,
+        contractAwardedForYears,
+        yearsContractSumVariousSpend
+      });
+  
+  
+    }).catch(err => {
+      console.log(err)
+      res.status(500).json({
+        err
+      })
+    })
+  }catch(err){
+    console.log(err)
+  }
+  
+  });
+  
 app.get("/*", (req, res) => {
   const app = ReactDOMServer.renderToString( < App / > );
 
@@ -36,6 +78,48 @@ app.get("/*", (req, res) => {
 });
 }else{
   app.use(express.static("./build"));
+  
+app.get("/api", (req, res) => {
+  try{
+    console.log(dbConfig)
+    oracledb.getConnection({
+        user: dbConfig.user,
+        password: dbConfig.password,
+        connectString: dbConfig.connectString
+      }).then(async conn => {
+      console.log(conn)
+      const currentYearContractSumQuery = 'select contractSubType, count(contractSubType) from reports_vendor group by contractSubType';
+      const currentYearSpendAreasQuery = 'select contractSubType, paymentType, count(contractSubType) from reports_vendor group by contractSubType group by paymentType';
+      const contractAwardedForYearsQuery = 'select contractSubType, trunc(requestDate, "Year") from reports_vendor group by contractSubType group by trunc(requestDate, "Year")'
+  
+      const currentYearContractSum = await conn.execute(currentYearContractSumQuery),
+        currentYearSpendAreas = await conn.execute(currentYearSpendAreasQuery),
+        liabilitiesInSpendAreas = [],
+        fecAwardedProjectsCurrentLiabilities = [],
+        contractAwardedForYears = await conn.execute(contractAwardedForYearsQuery),
+        yearsContractSumVariousSpend = [];
+  
+      res.json({
+        currentYearContractSum,
+        currentYearSpendAreas,
+        liabilitiesInSpendAreas,
+        fecAwardedProjectsCurrentLiabilities,
+        contractAwardedForYears,
+        yearsContractSumVariousSpend
+      });
+  
+  
+    }).catch(err => {
+      console.log(err)
+      res.status(500).json({
+        err
+      })
+    })
+  }catch(err){
+    console.log(err)
+  }
+  
+  });
   
 app.get("/*", (req, res) => {
   const app = ReactDOMServer.renderToString( < App / > );
@@ -55,63 +139,6 @@ app.get("/*", (req, res) => {
 }
 
 
-app.get("/api", (req, res) => {
-try{
-  console.log(dbConfig)
-  oracledb.getConnection({
-      user: dbConfig.user,
-      password: dbConfig.password,
-      connectString: dbConfig.connectString
-    }).then(async conn => {
-    console.log(conn)
-    const currentYearContractSumQuery = 'select contractSubType, count(contractSubType) from reports_vendor group by contractSubType';
-    const currentYearSpendAreasQuery = 'select contractSubType, paymentType, count(contractSubType) from reports_vendor group by contractSubType group by paymentType';
-    const contractAwardedForYearsQuery = 'select contractSubType, trunc(requestDate, "Year") from reports_vendor group by contractSubType group by trunc(requestDate, "Year")'
-
-    const currentYearContractSum = await conn.execute(currentYearContractSumQuery),
-      currentYearSpendAreas = await conn.execute(currentYearSpendAreasQuery),
-      liabilitiesInSpendAreas = [],
-      fecAwardedProjectsCurrentLiabilities = [],
-      contractAwardedForYears = await conn.execute(contractAwardedForYearsQuery),
-      yearsContractSumVariousSpend = [];
-
-    res.json({
-      currentYearContractSum,
-      currentYearSpendAreas,
-      liabilitiesInSpendAreas,
-      fecAwardedProjectsCurrentLiabilities,
-      contractAwardedForYears,
-      yearsContractSumVariousSpend
-    });
-
-
-  }).catch(err => {
-    console.log(err)
-    res.status(500).json({
-      err
-    })
-  })
-}catch(err){
-  console.log(err)
-}
-
-});
-
-app.get("/*", (req, res) => {
-  const app = ReactDOMServer.renderToString( < App / > );
-
-  const indexFile = path.resolve("../client/index.html");
-  fs.readFile(indexFile, "utf8", (err, data) => {
-    if (err) {
-      console.error("Something went wrong:", err);
-      return res.status(500).send("Oops, better luck next time!");
-    }
-
-    return res.send(
-      data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
-    );
-  });
-});
 
 app.listen(PORT, () => {
   console.log(`😎 Server is listening on port ${PORT}`);
