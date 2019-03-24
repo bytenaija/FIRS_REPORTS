24d48eb2143b361b55dce28ceb71bf3ec8fa4403
Everistus Olumese <everistusolumese@gmail.com>
2019-03-24 11:21:34 +0100

    Update

--- server-build/bundle.js
+++ server-build/bundle.js
@@ -22,7 +22,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "86d8d1535e9999573d58";
+/******/ 	var hotCurrentHash = "417c80d8897612a2f32b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -804,7 +804,7 @@ eval("(() => {\n  // Handle hot updates, copied with slight adjustments from web
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("module.exports = {\n  user: process.env.NODE_ORACLEDB_USER || \"weblogic\",\n  // Instead of hard coding the password, consider prompting for it,\n  // passing it in an environment variable via process.env, or using\n  // External Authentication.\n  password: process.env.NODE_ORACLEDB_PASSWORD || 'welcome1',\n  // For information on connection strings see:\n  // https://oracle.github.io/node-oracledb/doc/api.html#connectionstrings\n  connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || \"localhost/Revenue\",\n  // Setting externalAuth is optional.  It defaults to false.  See:\n  // https://oracle.github.io/node-oracledb/doc/api.html#extauth\n  externalAuth: process.env.NODE_ORACLEDB_EXTERNALAUTH = true\n};\n\n//# sourceURL=webpack:///./server/dbConfig.js?");
+eval("module.exports = {\n  user: process.env.NODE_ORACLEDB_USER || \"weblogic\",\n  // Instead of hard coding the password, consider prompting for it,\n  // passing it in an environment variable via process.env, or using\n  // External Authentication.\n  password: process.env.NODE_ORACLEDB_PASSWORD || 'welcome1',\n  // For information on connection strings see:\n  // https://oracle.github.io/node-oracledb/doc/api.html#connectionstrings\n  connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || \"localhost/bpm1\",\n  // Setting externalAuth is optional.  It defaults to false.  See:\n  // https://oracle.github.io/node-oracledb/doc/api.html#extauth\n  externalAuth: process.env.NODE_ORACLEDB_EXTERNALAUTH = true\n};\n\n//# sourceURL=webpack:///./server/dbConfig.js?");
 
 /***/ }),
 
--- server/dbConfig.js
+++ server/dbConfig.js
@@ -8,7 +8,7 @@ module.exports = {
 
   // For information on connection strings see:
   // https://oracle.github.io/node-oracledb/doc/api.html#connectionstrings
-  connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/Revenue",
+  connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/bpm1",
 
   // Setting externalAuth is optional.  It defaults to false.  See:
   // https://oracle.github.io/node-oracledb/doc/api.html#extauth
