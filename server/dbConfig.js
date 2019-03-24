8ade4b6ac4476ba5b4f2e19c723184a747845de5
Everistus Olumese <everistusolumese@gmail.com>
2019-03-24 11:46:10 +0100

    Update

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -8,9 +8,9 @@ module.exports = {
 
   // For information on connection strings see:
   // https://oracle.github.io/node-oracledb/doc/api.html#connectionstrings
-  connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "" ,//"localhost/bpm1",
+  connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "bpm1",
 
   // Setting externalAuth is optional.  It defaults to false.  See:
   // https://oracle.github.io/node-oracledb/doc/api.html#extauth
-  externalAuth: process.env.NODE_ORACLEDB_EXTERNALAUTH = true
+  externalAuth: process.env.NODE_ORACLEDB_EXTERNALAUTH = false 
 };
